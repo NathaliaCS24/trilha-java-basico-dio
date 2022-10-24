77aebf6be26e0f292a30c21e4212658769cc074d
NathaliaCS24 <comercial.nathaliacamargo@gmail.com>
2022-10-23 21:57:33 -0300

    Versao1 finalizada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Agência: ");
+        String agencia = sc.nextLine();
+        System.out.print("Conta: ");
+        int conta = sc.nextInt();
+        System.out.print("Nome do titular: ");
+        sc.nextLine();
+        String nome = sc.nextLine();
+        System.out.print("Qual o saldo inicial: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Obrigada por utilizar nosso Banco. Os dados da sua conta são:");
+        System.out.println("Agência: " + agencia + " - Conta: " + conta);
+        System.out.println("Nome do titular: " + nome);
+        System.out.printf("Saldo: %.2f", saldo);
+
+
+
+
     }
 }
